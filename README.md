7b22045324f983ae2349b7a6b54af697a749d1e1
Likuna <kapan22l@mtholyoke.edu>
2020-11-07 18:27:06 -0700

    application shell

---
+++ README.md
@@ -0,0 +1,19 @@
+# Intro to D#.js
+
+Starter code for the Intro to D3.js workshop
+
+Installations:
+* [Node & npm](https://www.npmjs.com/get-npm)
+* React - run "npm install react" in the terminal
+* D3 - run "npm i d3" in the terminal
+
+Resources:
+* [D3 Documentation](https://d3js.org/)
+* [Inspo](https://www.reddit.com/r/dataisbeautiful/)
+* Data
+    - [Kaggle](https://www.kaggle.com/datasets)
+    - [BigQuery](https://cloud.google.com/bigquery/public-data)
+
+
+Credits:
+* All hex codes are from [Color Of The Day](https://twitter.com/color_OTD)
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Intro to D3.js</h1>
+        <i>by Lia Kapanadze</i>
+      </header>
+      <div className="chart">
+        <h3>SVG Playground</h3>
+      </div>
+      <div className="chart">
+        <h3>Chart #1</h3>
+      </div>
+      <div className="chart">
+      <h3>Chart #2</h3>
+      </div>
+      <div className="chart">
+      <h3>Chart #3</h3>
+      </div>
+      <div className="chart">
+      <h3>Chart #4</h3>
+      </div>
+      <div className="chart">
+      <h3>Chart #5</h3>
+      </div>
+      <div className="chart">
+      <h3>Chart #6</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
