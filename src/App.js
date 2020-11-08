e77b83f351b55449cbd043d74e4d13e01023e70f
Likuna <kapan22l@mtholyoke.edu>
2020-11-07 19:06:29 -0700

    chart ids

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
         <h1>Intro to D3.js</h1>
         <i>by Lia Kapanadze</i>
       </header>
-      <div className="chart">
+      <div className="chart" id="chart1">
         <h3>SVG Playground</h3>
       </div>
-      <div className="chart">
+      <div className="chart" id="chart2">
         <h3>Chart #1</h3>
       </div>
-      <div className="chart">
+      <div className="chart" id="chart3">
       <h3>Chart #2</h3>
       </div>
-      <div className="chart">
+      <div className="chart" id="chart4">
       <h3>Chart #3</h3>
       </div>
-      <div className="chart">
+      <div className="chart" id="chart5">
       <h3>Chart #4</h3>
       </div>
-      <div className="chart">
+      <div className="chart" id="chart6">
       <h3>Chart #5</h3>
       </div>
-      <div className="chart">
+      <div className="chart" id="chart7">
       <h3>Chart #6</h3>
       </div>
     </div>
